817c3c214213baa27b998ecd0010aae14c2ccbae
Samuel F. Ademola <akatuski10x@gmail.com>
2019-06-28 14:47:15 -0400

    formating.

--- Project0plus/src/main/java/dev/cerebro/model/Account.java
+++ Project0plus/src/main/java/dev/cerebro/model/Account.java
@@ -1,4 +1,62 @@
 package dev.cerebro.model;
 
 public class Account {
+
+    private int aid;
+    private int cid;
+    private double amount;
+    private String created;
+
+    public Account() {
+        super();
+    }
+
+    public Account (int aid, int cid, double amount, String created) {
+        this.aid = aid;
+        this.cid = cid;
+        this.amount = amount;
+        this.created = created;
+    }
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "aid=" + aid +
+                ", cid=" + cid +
+                ", amount=" + amount +
+                ", created='" + created + '\'' +
+                '}';
+    }
 }
--- Project0plus/src/main/java/dev/cerebro/model/Transaction.java
+++ Project0plus/src/main/java/dev/cerebro/model/Transaction.java
@@ -1,4 +1,53 @@
 package dev.cerebro.model;
 
 public class Transaction {
+
+    private int tid;
+    private int aid;
+    private String type;
+    private double amount;
+    private String transaction_date;
+
+    public Transaction() {
+        super();
+    }
+
+    public Transaction(int tid, int aid, String type, double amount, String transaction_date) {
+        this.tid = tid;
+        this.aid = aid;
+        this.type = type;
+        this.amount = amount;
+        this.transaction_date = transaction_date;
+    }
+
+    public int getTid() { return tid; }
+
+    public void setTid(int tid) { this.tid = tid; }
+
+    public int getAid() { return aid; }
+
+    public void setAid(int aid) { this.aid = aid; }
+
+    public String getType() { return type; }
+
+    public void setType(String type) { this.type = type; }
+
+    public double getAmount() { return amount; }
+
+    public void setAmount(double amount) { this.amount = amount; }
+
+    public String getTransaction_date() { return transaction_date; }
+
+    public void setTransaction_date(String transaction_date) { this.transaction_date = transaction_date; }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "tid=" + tid +
+                ", aid=" + aid +
+                ", type='" + type + '\'' +
+                ", amount=" + amount +
+                ", transaction_date='" + transaction_date + '\'' +
+                '}';
+    }
 }

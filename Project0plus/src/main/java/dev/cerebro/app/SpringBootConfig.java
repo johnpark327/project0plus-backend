1417954035173c74870cbf409225f712afec9e95
Samuel F. Ademola <akatuski10x@gmail.com>
2019-07-04 15:11:27 -0400

    added the Spring Boot and the YML file.

--- Project0plus/src/main/java/dev/cerebro/app/SpringBootConfig.java
+++ Project0plus/src/main/java/dev/cerebro/app/SpringBootConfig.java
@@ -1,4 +1,14 @@
 package dev.cerebro.app;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
 public class SpringBootConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfig.class, args);
+    }
 }
